2d04c5d910d7fbb8c0ed17129ee05db1db62977e
Damian Gryski <damian@gryski.com>
2012-02-21 19:48:52 +0100

    A number of cleanups:
        - remove Auth* constants: caller doesn't need to know *how* user was
          athenticated
        - split main authenticate function into smaller pieces so we can
          unit test it better
        - change sized-ints to generic ints
        - a bunch more FIXMEs so I don't forget them

--- googauth.go
+++ googauth.go
@@ -17,8 +17,11 @@ import (
 	"time"
 )
 
+// FIXME: Much of this code assumes int == int64, which probably is not the case.
+
 // ComputeCode computes the response code for a 64-bit challenge 'value' using the secret 'secret'
-func ComputeCode(secret string, value uint64) int {
+// FIXME: really uint64?
+func ComputeCode(secret string, value int64) int {
 
 	key, _ := base32.StdEncoding.DecodeString(secret)
 
@@ -42,15 +45,6 @@ func (ErrInvalidCode) Error() string {
 	return "dgoogauth: invalid code"
 }
 
-type OTPAuthType int
-
-const (
-	AuthFail OTPAuthType = iota
-	AuthScratch
-	AuthHotp
-	AuthTotp
-)
-
 type OTPConfig struct {
 	Secret        string
 	WindowSize    int   // valid range: 0..100 or so
@@ -59,71 +53,50 @@ type OTPConfig struct {
 	ScratchCodes  []int
 }
 
-// FIXME: to faciliate unit testing, this function needs to be split up
-func (c *OTPConfig) Authenticate(password string) (OTPAuthType, error) {
+func (c *OTPConfig) checkScratchCodes(code int) (bool, error) {
 
-	var scratch bool
-
-	switch {
-	case len(password) == 6 && password[0] >= '0' && password[0] <= '9':
-		break
-	case len(password) == 8 && password[0] >= '1' && password[0] <= '9':
-		scratch = true
-		break
-	default:
-		return AuthFail, ErrInvalidCode(0)
-	}
-
-	code, err := strconv.Atoi(password)
-
-	if err != nil {
-		return AuthFail, ErrInvalidCode(0)
-	}
-
-	if scratch {
-		for i, v := range c.ScratchCodes {
-			if code == v {
-				// remove this code from the list of valid ones
-				l := len(c.ScratchCodes) - 1
-				c.ScratchCodes[i] = c.ScratchCodes[l] // copy last element over this element
-				c.ScratchCodes = c.ScratchCodes[0:l]  // and trim the list length by 1
-				return AuthScratch, nil
-			}
+	for i, v := range c.ScratchCodes {
+		if code == v {
+			// remove this code from the list of valid ones
+			l := len(c.ScratchCodes) - 1
+			c.ScratchCodes[i] = c.ScratchCodes[l] // copy last element over this element
+			c.ScratchCodes = c.ScratchCodes[0:l]  // and trim the list length by 1
+			return true, nil
 		}
-		return AuthFail, nil
 	}
 
-	// we have a counter value we can use
-	if c.HotpCounter > 0 {
-		for i := 0; i < c.WindowSize; i++ {
-			if ComputeCode(c.Secret, uint64(c.HotpCounter+i)) == code {
-				c.HotpCounter += i + 1
-				// We don't check for overflow here, which means you can only authenticate 2^63 times
-				// After that, the counter is negative and the above 'if' test will fail.
-				// This matches the behaviour of the PAM module.
-				return AuthHotp, nil
-			}
+	return false, nil
+}
+
+func (c *OTPConfig) checkHotpCode(code int) (bool, error) {
+
+	for i := 0; i < c.WindowSize; i++ {
+		if ComputeCode(c.Secret, int64(c.HotpCounter+i)) == code {
+			c.HotpCounter += i + 1
+			// We don't check for overflow here, which means you can only authenticate 2^63 times
+			// After that, the counter is negative and the above 'if' test will fail.
+			// This matches the behaviour of the PAM module.
+			return true, nil
 		}
-		// we must always advance the counter if we tried to authenticate with it
-		c.HotpCounter++
-		return AuthFail, nil
 	}
 
-	// assume we're on Time-basd OTP
+	// we must always advance the counter if we tried to authenticate with it
+	c.HotpCounter++
+	return false, nil
+}
 
-        // FIXME: needs to be split up so we can inject a time to test with
-	t0 := int(time.Now().Unix() / 30)
+func (c *OTPConfig) checkTotpCode(t0, code int) (bool, error) {
 
 	// FIXME: verify conditions are correct w/r/t valid window sizes
 	minT := t0 - (c.WindowSize / 2)
 	maxT := t0 + (c.WindowSize / 2)
 	for t := minT; t <= maxT; t++ {
-		if ComputeCode(c.Secret, uint64(t)) == code {
+		if ComputeCode(c.Secret, int64(t)) == code {
 
 			if c.DisallowReuse != nil {
 				for _, timeCode := range c.DisallowReuse {
 					if timeCode == t {
-						return AuthFail, nil
+						return false, nil
 					}
 				}
 
@@ -140,9 +113,43 @@ func (c *OTPConfig) Authenticate(password string) (OTPAuthType, error) {
 				c.DisallowReuse = c.DisallowReuse[min:]
 			}
 
-			return AuthTotp, nil
+			return true, nil
 		}
 	}
 
-	return AuthFail, nil
+	return false, nil
+}
+
+func (c *OTPConfig) Authenticate(password string) (bool, error) {
+
+	var scratch bool
+
+	switch {
+	case len(password) == 6 && password[0] >= '0' && password[0] <= '9':
+		break
+	case len(password) == 8 && password[0] >= '1' && password[0] <= '9':
+		scratch = true
+		break
+	default:
+		return false, ErrInvalidCode(0)
+	}
+
+	code, err := strconv.Atoi(password)
+
+	if err != nil {
+		return false, ErrInvalidCode(0)
+	}
+
+	if scratch {
+		return c.checkScratchCodes(code)
+	}
+
+	// we have a counter value we can use
+	if c.HotpCounter > 0 {
+		return c.checkHotpCode(code)
+	}
+
+	// assume we're on Time-basd OTP
+	t0 := int(time.Now().Unix() / 30)
+	return c.checkTotpCode(t0, code)
 }
