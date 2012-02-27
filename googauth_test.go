32ed6ca1d11710c157c67f0597f0326efa4be2a6
Damian Gryski <damian@gryski.com>
2012-02-27 21:35:21 +0100

    unit tests for time-based codes.

--- googauth_test.go
+++ googauth_test.go
@@ -95,3 +95,75 @@ func TestHotpCode(t *testing.T) {
 		}
 	}
 }
+
+func TestTotpCode(t *testing.T) {
+
+	var cotp OTPConfig
+
+	// reuse our test values from above
+	cotp.Secret = "2SH3V3GDW7ZNMGYE"
+	cotp.WindowSize = 5
+
+	var windowTest = []struct {
+		code   int
+		t0     int
+		result bool
+	}{
+		{50548, 9997, false},
+		{50548, 9998, true},
+		{50548, 9999, true},
+		{50548, 10000, true},
+		{50548, 10001, true},
+		{50548, 10002, true},
+		{50548, 10003, false},
+	}
+
+	for i, s := range windowTest {
+		r, e := cotp.checkTotpCode(s.t0, s.code)
+		if r != s.result {
+			t.Errorf("counterCode(%d) (step %d) failed: got %t expected %t", s.code, i, r, s.result)
+		}
+		if e != nil {
+			t.Errorf("weird error from checkHotpCode(%d): got %s", s.code, e)
+		}
+	}
+
+	cotp.DisallowReuse = make([]int, 0)
+	var noreuseTest = []struct {
+		code       int
+		t0         int
+		result     bool
+		disallowed []int
+	}{
+		{50548 /* 10000 */, 9997, false, []int{}},
+		{50548 /* 10000 */, 9998, true, []int{10000}},
+		{50548 /* 10000 */, 9999, false, []int{10000}},
+		{478726 /* 10001 */, 10001, true, []int{10000, 10001}},
+		{646986 /* 10002 */, 10002, true, []int{10000, 10001, 10002}},
+		{842639 /* 10003 */, 10003, true, []int{10001, 10002, 10003}},
+	}
+
+	for i, s := range noreuseTest {
+		r, e := cotp.checkTotpCode(s.t0, s.code)
+		if r != s.result {
+			t.Errorf("timeCode(%d) (step %d) failed: got %t expected %t", s.code, i, r, s.result)
+		}
+		if len(cotp.DisallowReuse) != len(s.disallowed) {
+			t.Errorf("timeCode(%d) (step %d) failed: disallowReuse len mismatch: got %t expected %t", s.code, i, len(cotp.DisallowReuse), len(s.disallowed))
+		} else {
+			same := true
+			for j := range s.disallowed {
+				if s.disallowed[j] != cotp.DisallowReuse[j] {
+					same = false
+				}
+			}
+			if !same {
+				t.Errorf("timeCode(%d) (step %d) failed: disallowReused: got %v expected %v", s.code, i, cotp.DisallowReuse, s.disallowed)
+			}
+		}
+
+		if e != nil {
+			t.Errorf("weird error from checkHotpCode(%d): got %s", s.code, e)
+		}
+	}
+}
